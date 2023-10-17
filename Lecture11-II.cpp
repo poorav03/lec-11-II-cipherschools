ffe79fe3381e72fe0a693d80f7341487051ff1b8
Poorav <118240170+poorav03@users.noreply.github.com>
2023-10-17 20:23:11 +0530

    Add files via upload

---
+++ Lecture11-II.cpp
@@ -0,0 +1,18 @@
+// Lecture 11 II - Address of Elements In C++ Pt 2
+#include <iostream>
+using namespace std;
+
+int main(){
+    long long int a;
+    int b;
+    char c[1];
+    float d;
+    double e;
+    cout << "The addresss of a : "<< &a << endl;
+    cout << "The addresss of b : "<< &b << endl;
+    cout << "The addresss of c : "<< &c << endl;
+    cout << "The addresss of d : "<< &d << endl;
+    cout << "The addresss of e : "<< &e << endl;
+
+    return 0;
+}
